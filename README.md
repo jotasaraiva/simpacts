4770bfad295b5f7a4ac59ddd921528c57885ba03
jotasaraiva <jpsaraiva2002@gmail.com>
2025-02-18 19:33:38 -0300

    tests different smoothing methos

---
+++ README.md
@@ -0,0 +1,3 @@
+The `src/` directory contains all the modules made for this research.
+The `data/` directory contains Jupyter notebooks about how to retrieve and prepare the necessary data.
+The `demo.ipynb` notebook shows all the calculations and respective results.
--- src/denoising.py
+++ src/denoising.py
@@ -1,4 +1,4 @@
-from skimage.restoration import denoise_wavelet, cycle_spin, denoise_tv_chambolle
+from skimage.restoration import denoise_wavelet, cycle_spin, denoise_tv_chambolle, denoise_nl_means
 import numpy as np
 
 def denoise_swt(img, wavelet='haar', J=2):
@@ -26,7 +26,7 @@ def apply_wavelet(x, show_progress=True):
             print("Applying Wavelet: ", str(i+1), "/", str(t), end="\r")
     return xwav
 
-def apply_tv(x, w=1.5):
+def apply_tv(x, w=1.5, show_progress=True):
     xtv = np.ndarray(x.shape)
     t = xtv.shape[0]
     for i in range(t):
@@ -34,5 +34,15 @@ def apply_tv(x, w=1.5):
             x[i, :, :],
             weight=w
         )
-        print(str(i+1)+"/"+str(t), end="\r")
+        if show_progress:
+            print("Applying TV: ", str(i+1), "/", str(t), end="\r")
     return xtv
+
+def apply_nlm(x, show_progress = True):
+    xnlm = np.ndarray(x.shape)
+    t = xnlm.shape[0]
+    for i in range(t):
+        xnlm[i, :, :] = denoise_nl_means(x[i, :, :])
+        if show_progress:
+            print("Applying NLM: ", str(i+1), "/", str(t), end="\r")
+    return xnlm
