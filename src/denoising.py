a20a8b91bd2941c226cc8354e41a6feb6ccd29fe
jotasaraiva <jpsaraiva2002@gmail.com>
2025-03-07 23:28:26 -0300

    Adds bilateral test

--- src/denoising.py
+++ src/denoising.py
@@ -1,4 +1,4 @@
-from skimage.restoration import denoise_wavelet, cycle_spin, denoise_tv_chambolle, denoise_nl_means
+from skimage.restoration import denoise_wavelet, cycle_spin, denoise_tv_chambolle, denoise_nl_means, denoise_bilateral
 import numpy as np
 
 def denoise_swt(img, wavelet='haar', J=2):
@@ -46,3 +46,12 @@ def apply_nlm(x, show_progress = True):
         if show_progress:
             print("Applying NLM: ", str(i+1), "/", str(t), end="\r")
     return xnlm
+
+def apply_bil(x, show_progress = True):
+    xbil = np.ndarray(x.shape)
+    t = xbil.shape[0]
+    for i in range(t):
+        xbil[i, :, :] = denoise_bilateral(x[i, :, :])
+        if show_progress:
+            print("Applying Bilateral: ", str(i+1), "/", str(t), end="\r")
+    return xbil
